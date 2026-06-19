ad705b90586f131f90afc6dd5c72089a95b73864
pedrotrevisan <52826302+pedrotrevisan@users.noreply.github.com>
2026-06-19 20:16:26 +0000

    Adicionar entidade User com Role e projecao UserDetailsProjection

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,77 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public User() {}
+
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.password = password;
+	}
+
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+	public String getPhone() { return phone; }
+	public void setPhone(String phone) { this.phone = phone; }
+	public LocalDate getBirthDate() { return birthDate; }
+	public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+	public List<Order> getOrders() { return orders; }
+
+	public void addRole(Role role) { roles.add(role); }
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() { return roles; }
+
+	@Override
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+
+	@Override
+	public String getUsername() { return email; }
+}
