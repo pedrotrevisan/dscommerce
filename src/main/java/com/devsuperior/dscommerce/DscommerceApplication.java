e807e91ef6d2af678d96ebbc5d27e4c4cd4dd707
pedrotrevisan <52826302+pedrotrevisan@users.noreply.github.com>
2026-06-19 20:16:13 +0000

    Inicializar projeto Spring Boot com estrutura base

---
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+.git/public static void main(String[] args) {
+.git/SpringApplication.run(DscommerceApplication.class, args);
+.git/}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,34 @@
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Maria Brown', 'maria@gmail.com', '11912341234', '2001-07-25', '$2a$10$nt4I9GVb2sEfMgEQGVoqme.vdLBFnpjVCX9KMJjH5hOeMSFQqW0bm');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Alex Green', 'alex@gmail.com', '11982842844', '1987-12-13', '$2a$10$nt4I9GVb2sEfMgEQGVoqme.vdLBFnpjVCX9KMJjH5hOeMSFQqW0bm');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Computers');
+
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('The Lord of the Rings', 90.5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Smart TV', 2190.0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Macbook Pro', 1250.0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer', 1200.0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Rails for Dummies', 100.99, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg');
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 2);
+
+INSERT INTO tb_order (moment, status, client_id) VALUES ('2022-07-25T13:00:00Z', 'PAID', 1);
+INSERT INTO tb_order (moment, status, client_id) VALUES ('2022-07-26T11:00:07Z', 'WAITING_PAYMENT', 1);
+INSERT INTO tb_order (moment, status, client_id) VALUES ('2022-07-27T14:11:07Z', 'SHIPPED', 2);
+
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (1, 1, 2, 90.5);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (1, 3, 1, 1250.0);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (2, 3, 2, 1250.0);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (3, 5, 2, 100.99);
